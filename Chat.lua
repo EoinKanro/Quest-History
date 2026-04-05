d713ecfd159c1a12faac62fe2d4e52fe31fdd9e7
EoinKanro <54404008+EoinKanro@users.noreply.github.com>
2026-04-05 03:45:38 +0200

    Refactor

---
+++ Chat.lua
@@ -0,0 +1,29 @@
+if not QH then QH = {} end
+if not QuestHistorySettingsDB then QuestHistorySettingsDB = {} end
+
+function QH.LogInfo(message)
+    print("|cFF00FF00[QH]:|r " .. message)
+end
+
+function QH.LogError(message)
+    print("|cFFFF0000[QH Error]:|r " .. message)
+end
+
+-- =========================
+-- Enable or disable chat logging
+-- =========================
+function QH.ReloadChatLogging(value)
+    if value == nil then
+        value = QuestHistorySettingsDB.enableLogBackup
+    end
+    if value == nil then
+        value = true
+    end
+
+    LoggingChat(value)
+    if value == true then
+        QH.LogInfo("Chat logging activated")
+    else
+        QH.LogInfo("Chat logging deactivated")
+    end
+end
